3e818303af568b04e4469286f57b03739dc7855c
Elias Vasques <eliasvasques74@gmail.com>
2023-11-23 17:49:51 -0300

    feat: Create login features: redirect to sign up, call auth api, display erros messages if any, redirect to home if login was sucessful

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import axios, { AxiosError } from 'axios';
+import { RouterLink, Router } from '@angular/router';
 
+interface LoginDto {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string | undefined = undefined;
+
+  constructor(private router: Router) {}
+
+  async handleLogin() {
+    if (this.password.length < 8 || !this.email.includes('@')) {
+      this.errorMessage = 'Dados incorretos';
+      return;
+    }
+
+    const body: LoginDto = {
+      email: this.email,
+      password: this.password,
+    };
+
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/auth/login',
+        body
+      );
+      if (response.data === true) {
+        this.router.navigate(['/']);
+      }
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 400)
+          this.errorMessage = 'Dados incorretos';
+        else if (error.response?.data?.message)
+          this.errorMessage = error.response.data.message;
+        else this.errorMessage = 'Erro no servidor';
+      }
+    }
+  }
+}
